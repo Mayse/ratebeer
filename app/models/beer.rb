a462466cda3497f1fbb95133e22025f949f074e5
Miika Säynevirta <mayse@iki.fi>
2016-01-26 13:16:16 +0200

    Viikko 2 Tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+
+		arr = []
+		Rating.all.each { |rating| arr << rating.score }
+		a = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+		a.round
+	end
 end
