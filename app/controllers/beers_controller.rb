a77640cf3dea2480150eed9aa5c759c8c811b767
Miika Säynevirta <mayse@iki.fi>
2016-01-30 11:46:39 +0200

    Viikko 2, Tehtävä 10

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -16,10 +16,13 @@ class BeersController < ApplicationController
   def new
     @beer = Beer.new
     @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
   end
 
   # GET /beers/1/edit
   def edit
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
+    @breweries = Brewery.all
   end
 
   # POST /beers
