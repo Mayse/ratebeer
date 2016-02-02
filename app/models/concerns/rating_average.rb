0379a7371a1df1b9cc4665fb14a04eef1b4f3817
Miika Säynevirta <mayse@iki.fi>
2016-02-02 13:55:27 +0200

    Viikko 3, Tehtävä 1

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -1,6 +1,7 @@
 module RatingAverage
 	extend ActiveSupport::Concern
 	def average_rating
+		return 0 if ratings.empty?
 		arr = []
 		self.ratings.each { |rating| arr << rating.score }
 		a = arr.inject{ |sum, el| sum + el }.to_f / arr.size
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
   resources :ratings, only: [:index, :new, :create]
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
+  get 'signup', to: 'users#new'
   root 'breweries#index'
 
  # get 'ratings', to: 'ratings#index'
