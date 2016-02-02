f4f6df40156120d32fd2da8ac704bc7f03ed6abc
Miika Säynevirta <mayse@iki.fi>
2016-02-02 16:01:40 +0200

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@
 class User < ActiveRecord::Base
 	include RatingAverage
 
+	#validates :username, uniqueness: true
+	#validates :username, length: { minimum: 3 }
+	validates :username, uniqueness: true,
+		length: { minimum: 3 }
+
 	has_many :ratings   # käyttäjällä on monta ratingia
 end
