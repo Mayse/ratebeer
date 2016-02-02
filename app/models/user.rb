4e2a2314bf5f3ac1b04931f75e945e3337ba6b4e
Miika Säynevirta <mayse@iki.fi>
2016-02-02 17:10:29 +0200

    Viikko 3, Tehtävä 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,14 @@
 class User < ActiveRecord::Base
 	include RatingAverage
 
+	has_secure_password
+
 	#validates :username, uniqueness: true
 	#validates :username, length: { minimum: 3 }
 	validates :username, uniqueness: true,
 		  length: { minimum: 3, maximum: 15 }
+	validates :password, length: {minimum: 4},
+			     format: { with: /(.*[A-Z].*[0-9]|.*[0-9].*[A-Z])/, message: "Must contain at least one number and capital letter"}
 
 	has_many :ratings   # käyttäjällä on monta ratingia
 end
