03fb748fdba4295827a2444433e945ffd9ce9bad
Miika Säynevirta <mayse@iki.fi>
2016-02-02 17:50:31 +0200

    Viikko 3, Tehtävä 12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,6 +58,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
 	  if current_user == @user
+		  session.destroy
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
