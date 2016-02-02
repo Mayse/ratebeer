05c2c4ede6a59eab8daee90978513188ba09b0c9
Miika Säynevirta <mayse@iki.fi>
2016-02-02 14:26:21 +0200

    Viikko 3, Tehtävä 2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   resource :session, only: [:new, :create, :destroy]
 
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   root 'breweries#index'
 
