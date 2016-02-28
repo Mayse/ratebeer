a034390f649f9aa962cd362aacc914e24e7f54ce
Miika Säynevirta <mayse@iki.fi>
2016-02-28 21:35:07 +0200

    Restrict only admins to delete breweries, beers, styles and beer clubs via helper function and prefilters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,15 @@ helper_method :current_user
 		return nil if session[:user_id].nil?
 		User.find(session[:user_id])
 	end
+	def admin?
+		return nil if current_user.nil?
+		current_user.admin
+	end
 
 	def ensure_that_signed_in
 		redirect_to signin_path, notice:'you should be signed in' if current_user.nil?
   	end
+	def ensure_that_is_admin
+		redirect_to signin_path, notice:'you should be admin to do that' unless current_user.admin?
+	end
 end
